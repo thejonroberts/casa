71f729ebbe29acbfa0ba69f62462f9ed6dce6fb3
Jon Roberts <jon.roberts.8@gmail.com>
2024-09-17 16:52:07 -0400

    form improvements

--- app/javascript/controllers/casa_nested_form_controller.js
+++ app/javascript/controllers/casa_nested_form_controller.js
@@ -1,20 +1,28 @@
 // https://www.stimulus-components.com/docs/stimulus-rails-nested-form/
 import NestedForm from '@stimulus-components/rails-nested-form'
 
-// NOTE: For some reason, using the base nested-form for multiple nested attribute forms in case_contacts
-// did not work. This was added to fix that, even though it appears to add no functionality.
-// It can be used in place of nested-form, and is particularly useful for debugging nested form events.
+// NOTE: Using the base nested-form for multiple nested attribute forms in case_contacts form
+// did not work. This was added to fix that, and adds a simple confirmation dialog for removing items.
+// It can be used in place of nested-form, and is also useful for debugging rails-nested-form.
+
 // Connects to data-controller="casa-nested-form"
 export default class extends NestedForm {
   connect () {
     super.connect()
   }
 
-  add = (event) => {
-    super.add(event)
+  add (e) {
+    super.add(e)
   }
 
-  remove = (event) => {
-    super.remove(event)
+  remove (e) {
+    super.remove(e)
+  }
+
+  windowConfirmRemove (e) {
+    const text = 'Are you sure you want to remove this item?'
+    if (window.confirm(text)) {
+      this.remove(e)
+    }
   }
 }
